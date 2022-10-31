70efd04bfd05600635b66f1659d7cf010c8379d8
jweinsteiniit <jweinstein@hawk.iit.edu>
2022-10-31 10:26:44 -0500

    added js behavior

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
+  var divButton = document.getElementById('entrybutton');
+  divButton.addEventListener('click',nameAlert);
+}
 window.addEventListener('load', init);
+function nameAlert(){
+  var text = document.getElementById('entryinput').value;
+  var h2result = document.getElementById('textoutput');
+  h2result.textContent = text;
+  alert("Jake Weinstein: "+text);
+}
