f30a7159ebdb004b51e0086ec145109283275fcb
jweinsteiniit <jweinstein@hawk.iit.edu>
2022-11-02 08:56:53 -0500

    moved function inside of init function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
+  function nameAlert(){
+    var text = document.getElementById('entryinput').value;
+    var h2result = document.getElementById('textoutput');
+    h2result.innerHTML = text;
+    alert("Jake Weinstein: "+text);
+  }
   var divButton = document.getElementById('entrybutton');
   divButton.addEventListener('click',nameAlert);
 }
 window.addEventListener('load', init);
-function nameAlert(){
-  var text = document.getElementById('entryinput').value;
-  var h2result = document.getElementById('textoutput');
-  h2result.innerHTML = text;
-  alert("Jake Weinstein: "+text);
-}
